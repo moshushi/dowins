67badf337c6b1c4652bc21643d525897a0f2c388
moshushi <moshushi@gmail.com>
2017-02-07 22:54:00 +0200

    some error find it

--- dowins/dowins.py
+++ dowins/dowins.py
@@ -11,10 +11,10 @@ from ast import literal_eval
 
 BASE_URL = u'https://www.instagram.com/'
 BASE_SUFFIX_POST = u'p/'
-NAME_ACCOUNT = "abc"
+# NAME_ACCOUNT = "abc"
 # NAME_ACCOUNT = "polovinkinandrey"
 # NAME_ACCOUNT = "NBA"
-# NAME_ACCOUNT = "frenzytechnix"
+NAME_ACCOUNT = "frenzytechnix"
 
 # TEST_SOME_DICT = {u'code': u'BQA3H09gCPm', u'dimensions': {u'width': 1080, u'height': 1350}, u'caption': u'Kiev TV tower.\n#\u043a\u0438\u0457\u0432 #\u043a\u0438\u0435\u0432 #kyiv #kiev #tvtower #televisiontower', u'comments_disabled': False, u'comments': {u'count': 0}, u'date': 1486047613, u'likes': {u'count': 24}, u'owner': {u'id': u'43237241'}, u'thumbnail_src': u'https://scontent-fra3-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/c0.135.1080.1080/16465279_1351964604878196_3035221119993905152_n.jpg?ig_cache_key=MTQ0MTM5NDMxMTIxOTM4OTQxNA%3D%3D.2.c', u'is_video': False, u'id': u'1441394311219389414', u'display_src': u'https://scontent-fra3-1.cdninstagram.com/t51.2885-15/e35/16465279_1351964604878196_3035221119993905152_n.jpg?ig_cache_key=MTQ0MTM5NDMxMTIxOTM4OTQxNA%3D%3D.2'}
 TEST_SOME_DICT = {u'code': u'BOPfznag78S', u'dimensions': {u'width': 1080, u'height': 1080}, u'caption': u'\u0418\u0433\u0440\u0430\u0435\u043c \u0441 \u041a\u043e\u0441\u0442\u0435\u0439 \u0432 \u043d\u0430\u0441\u0442\u043e\u043b\u044c\u043d\u0443\u044e \u0440\u043e\u043b\u0435\u0432\u043a\u0443 Hero Kids. \u0413\u041c \u0438\u0437 \u043c\u0435\u043d\u044f \u0442\u0430\u043a \u0441\u0435\u0431\u0435, \u0434\u0430 \u0435\u0449\u0435 \u0438 \u0441 \u043a\u0443\u0431\u0438\u043a\u0430\u043c\u0438 \u041a\u043e\u0441\u0442\u0435 \u043d\u0435 \u0432\u0435\u0437\u043b\u043e, \u043d\u043e \u0442\u0435\u043c \u043d\u0435 \u043c\u0435\u043d\u0435\u0435 \u0434\u0435\u0442\u0438-\u043f\u0440\u0438\u043a\u043b\u044e\u0447\u0435\u043d\u0446\u044b \u0433\u0435\u0440\u043e\u0438\u0447\u0435\u0441\u043a\u0438 \u043f\u0440\u0435\u0432\u043e\u0437\u043c\u043e\u0433\u043b\u0438 \u043a\u0440\u044b\u0441\u0438\u043d\u043e\u0433\u043e \u043a\u043e\u0440\u043e\u043b\u044f ;)\n#herokids #roleplay #boardgame #pnprpg', u'comments_disabled': False, u'comments': {u'count': 1}, u'date': 1482243738, u'likes': {u'count': 22}, u'owner': {u'id': u'43237241'}, u'thumbnail_src': u'https://scontent-bru2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/15338486_1551801051502691_5653328674196291584_n.jpg?ig_cache_key=MTQwOTQ4NTA5NDg0NjE4NTIzNA%3D%3D.2', u'is_video': False, u'id': u'1409485094846185234', u'display_src': u'https://scontent-bru2-1.cdninstagram.com/t51.2885-15/e35/15338486_1551801051502691_5653328674196291584_n.jpg?ig_cache_key=MTQwOTQ4NTA5NDg0NjE4NTIzNA%3D%3D.2'}
@@ -82,7 +82,8 @@ def remake(some_dict):
     """
     for key in some_dict:
         pass
-#         print key
+        print key
+    print some_dict[u'code']
 #     print some_dict[u'comments']
 
     # remove some pair from dictionary
@@ -138,6 +139,7 @@ def process_unit_page(url):
     unit_row = get_unit_rawdata_from_html(html)
     data_li = get_dict_from_unit(unit_row)
 # #     print change_obg_in_data(data_li)
+    print change_obg_in_data(data_li)
     return change_obg_in_data(data_li)
 
 
@@ -208,10 +210,10 @@ def get_comments(url):
 
 def main():
 # #     process_unit_page(BASE_URL + NAME_ACCOUNT)
-    print get_comments(URL_ONE_PAGE)
+#     print get_comments(URL_ONE_PAGE)
 # #     remake(TEST_SOME_DICT)
 #     remake_comment(TEST_COMM_DICT)
-#     process_unit_page(BASE_URL+NAME_ACCOUNT)
+    process_unit_page(BASE_URL+NAME_ACCOUNT)
     pass
 
 if __name__ == '__main__':
