a7ddde9524f3e6f632d162472a36dba09f3ae267
moshushi <moshushi@gmail.com>
2017-08-03 16:17:10 +0300

    deep clean

--- tests/test_d.py
+++ tests/test_d.py
@@ -5,12 +5,6 @@ import json
 import requests_mock
 import dowins
 
-r = requests.get('https://twitter.com')
-print (r.status_code)
-print (r.cookies)
-# print (r.header) ### !not correct
-print ('----')
-
 
 with open('new_cookie_string_jar.txt', 'r') as f:
     my_cookies_str = f.readline()
@@ -20,49 +14,11 @@ with open('new_cookie_string_jar.txt', 'r') as f:
 with open('new_cookie_string.txt', 'r') as s:
     str_cookie = s.readline()
 
-with requests_mock.mock() as m:
-    m.get('https://www.instagram.com', status_code=201,
-          cookies={'csrftoken': my_cookies_dic},
-          headers={'set-cookie': str_cookie}
-          )
-#     r = requests.get('https://www.instagram.com')
-#     print (r.status_code)
-#     print (r.cookies)
-#     print (r.headers)
-#     print ('~~~~')
-#     print (r.headers['set-cookie'])
-#     print ('~~~~')
-#     print (r.cookies['csrftoken'])
-#     print ('====')
-# #     print (dowins.PostsExtractor.get_csrf_and_cookie_string()[0])
-    d =  dowins.PostsExtractor.get_csrf_and_cookie_string()[0]
-    print ('~~~~')
-    print(d)
-    print ('====')
-#     print(my_cookies_str)
-    print(my_cookies_dic)
-    assert dowins.PostsExtractor.get_csrf_and_cookie_string()[0] == my_cookies_dic
+def test_Q():
+    with requests_mock.mock() as m:
+        m.get('https://www.instagram.com', status_code=201,
+            cookies={'csrftoken': my_cookies_dic},
+            headers={'set-cookie': str_cookie}
+            )
 
-
-# httpretty.enable()
-# httpretty.register_uri(httpretty.GET, "https://twitter.com/",
-#                        body="TEST", status=201, cookies=my_cookies_jar,
-#                        adding_headers={
-#                            'set-cookie': str_cookie,
-#                            'Host': 'twitter.com'
-#                        })
-#
-# r = requests.get('https://twitter.com')
-# print (r.status_code)
-# print (r.cookies)
-# print (r.headers)
-# # print (type(r.cookies))
-# # httpretty.disable()
-#
-# # print("~~~~~")
-# # print(my_cookies_jar)
-# # print(type(my_cookies_jar))
-# # print("~~~~~")
-# # print(r.headers)
-#
-# #                        cookies = my_cookies_jar,
+        assert dowins.PostsExtractor.get_csrf_and_cookie_string()[0] == my_cookies_dic
--- tests/test_e.py
+++ tests/test_e.py
@@ -15,26 +15,3 @@ def test_B(data_small_p):
 
         assert dowins.PostsExtractor.extract_user_profile('polovinkinandrey')[0] == str(1577167408)
         assert dowins.PostsExtractor.extract_user_profile('polovinkinandrey')[1] == str(6)
-
-
-# def test_C():
-#     with requests_mock.Mocker() as m:
-#         m.get('https://www.instagram.com/', cookies={"csrf":"ESFxG4"})
-#         m.get('https://www.instagram.com/, real_http=True')
-#         assert dowins.PostsExtractor.get_csrf_and_cookie_string()[0] == "AEB"
-#     return(requests.get('https://instgagram.com').status_code)
-#         baar = dowins.PostsExtractor.get_csrf_and_cookie_string()
-#     return baar
-#     return(dowins.PostsExtractor.get_csrf_and_cookie_string())
-#     return 5
-
-def test_D():
-#     requests_mock.adapter.register_uri('GET', 'mock://test.com/7', cookies={'foo': 'bar'})
-#     resp = session.get('mock://test.com/7')
-#     return resp.cookies['foo']
-#     return requests_mock.adapter
-    pass
-
-if __name__ == '__main__':
-    a = test_D()
-    print(a)
